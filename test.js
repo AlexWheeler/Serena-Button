a3f967e64d23b7060a5d607945cc85113f5adf9d
Alex <awheeler@bu.edu>
2014-07-11 09:29:06 -0400

    sets up serial connection with Arduino -> succesful

--- test.js
+++ test.js
@@ -1,6 +1,38 @@
-chrome.serial.getDevices(function(ports) {
-  var numberOfPorts = ports.length
-  for(i=0; i < numberOfPorts; i++) {
-    console.log(ports[i].path);
+//sets global variable to check if connection failed by not returning a connectionId
+var connectionId = -1
+
+//gets the path of the serial port we want to connect to
+function getPortPath(ports) {
+    var div = document.getElementById('serial-port');
+    var portPath = ports[5].path
+    div.innerHTML = portPath;
+}
+
+//Sets connection status oh html
+function setConnectionStatus(connectionId, successStatus) {
+  var statusDiv = document.getElementById('status')
+
+  if (connectionId == -1) {
+    statusDiv.innerHTML = 'Connection failed!'
   }
-})
+  else if (connectionId > 0) {
+    statusDiv.innerHTML = successStatus + ' with connection ID: ' + connectionId
+  }
+}
+
+//tell App what to on succesful connection to serial port
+function onOpen(openData) {
+  var connectionId = openData.connectionId;
+  setConnectionStatus(connectionId, 'Succesfully connected');
+}
+
+//opens serial connection
+function openPort() {
+  var connectedPort = document.getElementById('serial-port').innerHTML
+  chrome.serial.connect(connectedPort, onOpen);
+}
+
+chrome.serial.getDevices(function(ports) {
+  getPortPath(ports);
+  openPort();
+});
